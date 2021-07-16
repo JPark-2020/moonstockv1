bdaa5568dbd9fe93f059b942bb5796f3a2e7cf70
jonathancode <parkj97@gmail.com>
2021-07-15 23:21:24 -0700

    db and models

---
+++ models/index.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.MONGODB_URI || "mongodb://localhost:27017/moonstock", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Mongo connection open!");
+  })
+  .catch((err) => {
+    console.log("Error!");
+    console.log(err);
+  });
+
+module.exports = {
+  stocks: require("./stocks.js"),
+  user: require("./user.js"),
+};
---
+++ models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+//convert schema to model
+const user = mongoose.model(
+  "users",
+  new mongoose.Schema({
+    username: String,
+    password: String,
+    cash: Number,
+    stocks: [
+      {
+        stock: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Stock",
+        },
+        amount: Number,
+      },
+    ],
+    watchlist: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Stock",
+      },
+    ],
+  })
+);
+
+module.exports = user;
